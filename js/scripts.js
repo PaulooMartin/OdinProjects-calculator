3ec577bd0e03512405dd45c8cecb619c1b59ef74
Paul Martin <paulmartinblopez@gmail.com>
2023-04-13 11:37:19 +0800

    Add temp user-prompts for numbers and operator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+let numberA = +prompt("Enter a number:")
+let numberB = +prompt("Enter a second number:")
+let operator = prompt("What do you want to do with these?", "add/subtract/multiply/divide")
+
 function add (x,y){
     return x + y;
 }
