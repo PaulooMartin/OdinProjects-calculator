bff7e0fb2263684453e8d1d0fdb59a6e04981d4c
Paul Martin <paulmartinblopez@gmail.com>
2023-04-13 17:00:15 +0800

    Unimportant - change vscode formatter

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,68 @@
-let numbers = []
-let operators = []
+let numbers = [];
+let operators = [];
 
 const digitBtns = document.querySelectorAll(".number");
 numListener(digitBtns);
 
-function operate (first, second, operator){
-    switch (true){
-        case (operator === "+"):
-            return add(first,second)
+function operate(first, second, operator) {
+  switch (true) {
+    case operator === "+":
+      return add(first, second);
 
-        case (operator === "-"):
-            return subtract(first,second)
+    case operator === "-":
+      return subtract(first, second);
 
-        case (operator === "*"):
-            return multiply(first,second)
+    case operator === "*":
+      return multiply(first, second);
 
-        case (operator === "/"):
-            return divide(first,second)
+    case operator === "/":
+      return divide(first, second);
 
-        default:
-            console.log("Something went wrong, operate()")
-        }
+    default:
+      console.log("Something went wrong, operate()");
+  }
 }
 
-function add (x,y){
-    return x + y;
+function add(x, y) {
+  return x + y;
 }
 
-function subtract (x,y){
-    return x - y;
+function subtract(x, y) {
+  return x - y;
 }
 
-function multiply (x,y){
-    return x * y;
+function multiply(x, y) {
+  return x * y;
 }
 
-function divide (x,y){
-    return x / y;
+function divide(x, y) {
+  return x / y;
 }
 
-function numListener (numNodeList){
-    numNodeList.forEach(numBtn => {
-        numBtn.addEventListener('click', () => {
-        console.log(numBtn.attributes["data-digit"].value)
-    })
-})
+function numListener(numNodeList) {
+  numNodeList.forEach((numBtn) => {
+    numBtn.addEventListener("click", () => {
+      console.log(numBtn.attributes["data-digit"].value);
+    });
+  });
 }
 // Simulates a calculator that is able to do multiple operations with multiple numbers
 // Inputs only taken from prompt()s, outputs result to the console
 // No checksum for invalid inputs
-function TEMP(){
-    do{
+function TEMP() {
+  do {
     numbers.push(+prompt("Enter a number:"));
-    operators.push(prompt("Enter an operator: ", "Enter '=' if you are finished."));
-    } while (operators[operators.length - 1] != "=")
-
-    let finalResult = numbers.reduce((result, currentNumber, currentIndex) => operate(result, currentNumber, operators[currentIndex-1])
+    operators.push(
+      prompt("Enter an operator: ", "Enter '=' if you are finished.")
     );
+  } while (operators[operators.length - 1] != "=");
 
-    numbers = [];
-    operators = [];
+  let finalResult = numbers.reduce((result, currentNumber, currentIndex) =>
+    operate(result, currentNumber, operators[currentIndex - 1])
+  );
 
-    console.log(finalResult)
+  numbers = [];
+  operators = [];
+
+  console.log(finalResult);
 }
