2cbdd735f259bdb9dcfdcb25ae315bbd378df1b7
Paul Martin <paulmartinblopez@gmail.com>
2023-04-13 11:45:40 +0800

    Add operate() and temp functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,29 @@ let numberA = +prompt("Enter a number:")
 let numberB = +prompt("Enter a second number:")
 let operator = prompt("What do you want to do with these?", "add/subtract/multiply/divide")
 
+function operate (first, second, operator){
+    switch (true){
+        case (operator === "add"):
+            alert(`Result is ${add(first,second)}`)
+            break;
+
+        case (operator === "subtract"):
+            alert(`Result is ${subtract(first,second)}`)
+            break;
+
+        case (operator === "multiply"):
+            alert(`Result is ${multiply(first,second)}`)
+            break;
+
+        case (operator === "divide"):
+            alert(`Result is ${divide(first,second)}`)
+            break;
+
+        default:
+            console.log("Something went wrong, operate()")
+        }
+}
+
 function add (x,y){
     return x + y;
 }
